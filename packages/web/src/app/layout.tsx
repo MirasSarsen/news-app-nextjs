a40382ac153036ee39cadbb7cea6e016fd32dfa3
Miras Sarsen <miras.frontender@gmail.com>
2025-11-29 19:40:56 +0500

    refactor(web): update layout structure

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -14,8 +14,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers>{children}</Providers>
+      <body>
+        <div className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <Providers>{children}</Providers>
+        </div>
       </body>
     </html>
   );
